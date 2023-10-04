e261c0eaa67a832a965401d182e59cfcc4bb6ea4
neasher1 <neasher75@gmail.com>
2023-10-04 13:01:04 +0600

    fix the terminate when users gives wrong arguments

--- StudentList.java
+++ StudentList.java
@@ -5,6 +5,12 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) {
 
+		//step 2
+		if (args == null || args[0].length() != 1) {
+			System.out.println("Please provide a, r, ?, +, or c argument");
+			return;
+		}
+
 		// Check arguments
 		//testing merge to another step
 		if (args[0].equals("a")) {
